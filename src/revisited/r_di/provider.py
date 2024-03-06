4e69ccba7f58e706e610c099b96450102cb84630
patrick <patrickwuke@163.com>
2024-03-06 14:35:21 +0800

    Add DI Examples

---
+++ src/revisited/r_di/provider.py
@@ -0,0 +1,28 @@
+from .exceptions import DoesNotBoundException
+
+
+class Provider:
+    def __init__(self):
+        self._bindings = {}
+
+    def bind(self, interface=None, impl=None, lazy: bool = False) -> None:
+        if lazy is False:
+            impl = impl()
+
+        self._bindings[interface] = impl
+
+    def unbind(self, interface) -> None:
+        """Unbind class"""
+        try:
+            self._bindings.pop(interface)
+        except KeyError:
+            raise DoesNotBoundException(cls_name=interface.__class__.__name__)
+
+    def clear_bindings(self) -> None:
+        """Clear bindings"""
+        self._bindings = {}
+
+    @property
+    def bindings(self) -> dict:
+        """Get current bindings"""
+        return self._bindings
